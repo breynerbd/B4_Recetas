06b26afba5a35ce41c37e3d1da00ba104ca950a2
Informatica <informatica@alumno.local>
2025-09-01 11:17:26 -0600

    Creacion de Repositorio y Proyecto

---
+++ src/test/java/org/los_buenos/recetas/RecetasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.los_buenos.recetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
